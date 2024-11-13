866690ccca6f296b53f7b49b04b4b3e18e53bb06
Rifat Abdullah <63234797+rifatabdullah@users.noreply.github.com>
2024-11-13 18:35:06 +0000

    Pending changes exported from your codespace

---
+++ as_2.java
@@ -0,0 +1,41 @@
+
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
+// class main{
+//     public static void main(String [] args){
+//         System.out.println("Hello ! World ");
+//     }
+// }
+
+class main{
+    public static void main (String[] args){
+        System.out.println("Here's the OutPut: ");
+        int i = 24;
+        while ( i >= -6 ){
+               
+            if ( i == -6 ){
+                System.out.print(i+" ");
+            }
+            else{
+                System.out.print( i+", ");
+            }
+            i -= 6;
+        }
+        
+
+    }
+}
+
+//  class SequenceUsingWhile {
+//     public static void main(String[] args) {
+//         System.out.println("\nSequence using while loop:");
+//         int i = 24;
+//         while (i >= -6) {
+//             System.out.print(i + " ");
+//             i -= 6;
+//         }
+//     }
+// }
+
+
+
